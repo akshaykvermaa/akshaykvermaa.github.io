3c4d20f2dbd635726baa98b5adcca035f4a84cf9
akshaykvermaa <akshaykvermaa@gmail.com>
2025-12-30 21:12:39 +0000

    Deploying to gh-pages from @ akshaykvermaa/akshaykvermaa.github.io@e72f5b0d4051c9d0f99c03cb4516d5d7733f1eac 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-research",
+  },{id: "nav-research",
           title: "research",
           description: "",
           section: "Navigation",
@@ -36,6 +29,13 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
+          },
+        },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
           },
         },{id: "nav-submenus",
           title: "submenus",
